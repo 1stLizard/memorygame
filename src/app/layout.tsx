2e6af0c22d8654b9d7702f7a02ea93874be3f6ad
1stLizard <jonhmaya673@gmail.com>
2025-07-23 20:21:16 -0600

    podrian ponerle las imagenes a las cartas Kevin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,17 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// ❌ No pongas "use client" aquí
+
 import "./globals.css";
+import { ReactNode } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Memorama App",
-  description: "Una colaboracion de Diseño Grafico e Ingienieria en Sistemas",
+  description: "Una colaboración de Diseño Gráfico e Ingeniería en Sistemas",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="bg-black text-white">{children}</body>
     </html>
   );
 }
